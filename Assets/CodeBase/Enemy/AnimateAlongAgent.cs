4ae38e3adadec229a0d04cde4862d158b6a5b24d
SkyShadowMeadow <catherine.semiletova@gmail.com>
2022-02-07 10:40:00 +0300

    Change of animations is set.

---
+++ Assets/CodeBase/Enemy/AnimateAlongAgent.cs
@@ -0,0 +1,26 @@
+﻿using CodeBase.Enemy;
+using UnityEngine;
+
+namespace Assets.CodeBase.Enemy
+{
+    [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof(EnemyAnimator))]
+    public class AnimateAlongAgent : MonoBehaviour
+    {
+        private const float MinimalVelocity = 0.1f;
+
+        [SerializeField] private UnityEngine.AI.NavMeshAgent Agent;
+        [SerializeField] private EnemyAnimator Animator;
+
+        private void Update()
+        {
+            if (ShouldMove())
+                Animator.Move(Agent.velocity.magnitude);
+            else
+                Animator.StopMoving();
+        }
+
+        private bool ShouldMove() =>
+          Agent.velocity.magnitude > MinimalVelocity && Agent.remainingDistance > Agent.radius;
+    }
+}
